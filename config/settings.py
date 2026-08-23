7d562abfdc8860f7cd2afbb92782889b6ab3ab8a
Suraj Pandey <83890644+surazzpande@users.noreply.github.com>
2026-08-23 16:34:10 +0100

    Add Render deployment config

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = [
     for h in os.environ.get(
         "DJANGO_ALLOWED_HOSTS",
         "localhost,127.0.0.1,.vercel.app,surajportfolio.com,www.surajportfolio.com",
+        "portfolio-g78n.onrender.com",
     ).split(",")
     if h.strip()
 ]
