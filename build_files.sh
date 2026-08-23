7e137f6f0584e3266579c6a0082a534c75351826
Suraj Pandey <83890644+surazzpande@users.noreply.github.com>
2026-08-23 12:09:21 +0100

    Fix Vercel build: allow pip install

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Vercel build step: install deps and collect static files.
 set -e
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python3 -m pip install --break-system-packages -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
